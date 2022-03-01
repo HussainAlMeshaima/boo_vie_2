84e270d6bfa94c0fc2d2cc91d1efba3269fbfc19
Hussain AlMeshaima <hussainbh00@gmail.com>
2022-03-01 22:24:58 +0300

    fix BookService

--- lib/app/services/book_service.dart
+++ lib/app/services/book_service.dart
@@ -1,18 +1,24 @@
+import 'dart:convert';
+
+import 'package:boo_vie/app/models/books/book_volume/book_volume.dart';
 import 'package:http/http.dart' as http;
 import 'package:injectable/injectable.dart';
 
-import '../models/books/list_volumes/list_volumes_error_model.dart';
 import '../models/books/list_volumes/list_volumes_model.dart';
 
 @lazySingleton
 class BookService {
   static const _prefix = 'https://www.googleapis.com/books/v1';
 
-  Future<dynamic> getListVolumes(String? q) async {
+  Future<dynamic> getListVolumes({required String q}) async {
     http.Response _response = await http.get(Uri.parse('$_prefix/volumes?q=$q'));
-    if (_response.statusCode != 200) {
-      return ListVolumesErrorModel.fromJson(_response.body);
-    }
+    if (_response.statusCode != 200) { return jsonDecode(_response.body); }
     return ListVolumesModel.fromJson(_response.body);
+  }
+
+  Future<dynamic> getBook({required String volumeId}) async {
+    http.Response _response = await http.get(Uri.parse('$_prefix/volumes/$volumeId'));
+    if (_response.statusCode != 200) { return jsonDecode(_response.body); }
+    return BookVolume.fromJson(_response.body);
   }
 }
