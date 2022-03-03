c23f56631ec502d7d48c1db5145d15ac82bb03c0
Hussain AlMeshaima <hussainbh00@gmail.com>
2022-03-03 23:37:50 +0300

    fix login

--- lib/ui/views/login/login_view.dart
+++ lib/ui/views/login/login_view.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:boo_vie/app/theme/colors.dart';
+import 'package:boo_vie/ui/views/login/widgets/forgot_password_text_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 import './login_view_model.dart';
+import 'widgets/login_button_widget.dart';
+import 'widgets/text_field_widget.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -10,22 +16,24 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<LoginViewModel>.reactive(
       viewModelBuilder: () => LoginViewModel(),
-      onModelReady: (LoginViewModel model) async {
-        await model.init();
-      },
-      builder: (
-        BuildContext context,
-        LoginViewModel model,
-        Widget? child,
-      ) {
-        return Scaffold(
-          body: Center(
-            child: Text(
-              'LoginView',
-            ),
+      onModelReady: (LoginViewModel model) async => await model.init(),
+      builder: (BuildContext context, LoginViewModel model, Widget? child) => GestureDetector(
+        onTap: () => model.removeFocus(),
+        child: Scaffold(
+          body: ListView(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            reverse: true,
+            children: const [
+              FlutterLogo(size: 120),
+              TextFieldWidget(hintText: 'Email'),
+              TextFieldWidget(hintText: 'Password'),
+              ForgotPAsswordTextButton(),
+              LoginButtonWidget(),
+            ].reversed.toList(),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
